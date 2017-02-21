76b73d8d66a53f8347d765b653e1f4c523475f1a
tchenbrian <brian.715218@nv.ccsd.net>
2017-02-21 08:31:24 -0800

    javascript update

--- quiz-template/_assets/script.js
+++ quiz-template/_assets/script.js
@@ -1,47 +1,54 @@
 // make result invisible
 
-var	results = document.getElementById('results');
+var results = document.getElementById('results');
 var quizForm = document.getElementById('quiz');
 var scoreResult = document.getElementById('scoreResult');
-results.style.display = 'none';
-var i;
 
 // check answers
 
 function showResult() {
-	var total = 10,
-			score = 0,
-			answers =  ["b","a","d","b","d","c","d","a","b","c"],
-            answerNames = ["red","a","d","b","d","c","d","a","b","c"];
 	
-	// Get user input
-	var q1 = document.forms['quizForm']['q1'].value;
-	var q2 = document.forms['quizForm']['q2'].value;
-	var q3 = document.forms['quizForm']['q3'].value;
-	var q4 = document.forms['quizForm']['q4'].value;
-	var q5 = document.forms['quizForm']['q5'].value;
-    var q1 = document.forms['quizForm']['q6'].value;
-	var q2 = document.forms['quizForm']['q7'].value;
-	var q3 = document.forms['quizForm']['q8'].value;
-	var q4 = document.forms['quizForm']['q9'].value;
-	var q5 = document.forms['quizForm']['q10'].value;
+    quiz.style.display = 'none';
+	results.style.display = 'block';
+                
+    var total = 10;
+    var score = 0;
+  
+  //Get user input
+ 
+ var q1 = document.forms['quizForm']['q1'].value;
+ var q2 = document.forms['quizForm']['q2'].value;
+ var q3 = document.forms['quizForm']['q3'].value;
+ var q4 = document.forms['quizForm']['q4'].value;
+ var q5 = document.forms['quizForm']['q5'].value;
+ var q6 = document.forms['quizForm']['q6'].value;
+ var q7 = document.forms['quizForm']['q7'].value;
+ var q8 = document.forms['quizForm']['q8'].value;
+ var q9 = document.forms['quizForm']['q9'].value;
+ var q10 = document.forms['quizForm']['q10'].value;
+  
+  // Validation  
+/*  for(var i = 1; i <= total; i++) {
+    if(eval('q' + i) === null || eval('q' + i) == '' ) {
+      alert('You missed question ' + i);
+      return false;
+    }
+  } */
+  
+  // Set correct answers
+  var answers = ["b","d","b","a","c","b","d","a","b","c"];
+  
+  for(var i = 1; i <= total; i++) {
+    // Check answers
+    if (eval('q' + i) == answers[i - 1]) {
+      score++;
+    }
+  }
 
-	// Validation
-/*	for(i=1; i<=total; i++) {
-		if(eval('q'+i) == null || eval('q'+i) == '') {
-			alert('You missed question #' + i);
-			return false;
-		}
-	} */
-	
-	// Check for Answers
-	for(i=1; i <= total; i++){
-		if(eval('q'+i) == answers[i -1]){
-				score++
-		}
-	}
+  // Display results
+  
     
-    var correctAnswerStatus = document.getElementsByClassName('correctAnswer');
+var correctAnswerStatus = document.getElementsByClassName('correctAnswer');
     
     for (i=0; i < correctAnswerStatus.length; i++) {
         
@@ -49,14 +56,20 @@ function showResult() {
             
     }
     
-  //  correctAnswerStatus.innerHTML(answers[i]);
+  var yourAnswerStatus = document.getElementsByClassName('yourAnswer');
+    
+    for (i=0; i < yourAnswerStatus.length; i++) {
+        
+        yourAnswerStatus[i].innerHTML += userAnswers[i];
+        
+    }
+        
+        var results = document.getElementById('scoreResult');
+  results.innerHTML = '<h3>You scored <span>' + score + '</span> out of <span>' + total + '</span></h3>';
+  //alert('You scored ' + score + ' out of ' + total);
 	
-    quizForm.style.display = 'none';
-	results.style.display = 'block';
 
-    scoreResult.innerHTML = 'Your Score is <strong>'+ score +'</strong> of <strong>'+ total+'</strong>';
-                
-	
+
+    
 }
-
 
